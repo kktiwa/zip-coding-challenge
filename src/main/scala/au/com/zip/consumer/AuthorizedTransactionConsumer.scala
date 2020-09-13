22f0c4efe7e5eb2364e6b913c13d493d7a22c31e
ktiwary <kunal.tiwary@quantium.com.au>
2020-09-13 16:56:47 +1000

    added notification service

--- src/main/scala/au/com/zip/consumer/AuthorizedTransactionConsumer.scala
+++ src/main/scala/au/com/zip/consumer/AuthorizedTransactionConsumer.scala
@@ -4,6 +4,7 @@ import java.time.Duration
 import scala.collection.JavaConverters._
 import au.com.zip.admin._
 import au.com.zip.encoders._
+import au.com.zip.service.{Notification, NotificationService}
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecords, KafkaConsumer}
 
 object AuthorizedTransactionConsumer extends App {
@@ -15,7 +16,7 @@ class PaymentGatewayConsumer {
   val props = createBaseProps()
   props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "au.com.zip.encoders.SimpleCaseClassDeserializer")
   props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "au.com.zip.encoders.SimpleCaseClassDeserializer")
-  props.put(ConsumerConfig.GROUP_ID_CONFIG, "TransactionConsumerTest")
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-gateway-consumer")
   val topics = Seq(cardAuthorizedTopic)
 
   val consumer: KafkaConsumer[CardRequestKey, CardAuthorizationResponse] = new KafkaConsumer[CardRequestKey, CardAuthorizationResponse](props)
@@ -26,6 +27,8 @@ class PaymentGatewayConsumer {
     val records: ConsumerRecords[CardRequestKey, CardAuthorizationResponse] = consumer.poll(Duration.ofMillis(100))
     records.iterator().asScala.foreach(record => {
       val key = record.key()
+      val response = record.value()
+      NotificationService.sendNotification(Notification(key.customerId, key.txnDateTime, response))
       println(s"Customer ${key.customerId} \t Request ${key.requestId} is ${record.value().status} ${Option(record.value().reason).map(s => s"Reason : $s").getOrElse("")}")
     })
   }
---
+++ src/main/scala/au/com/zip/service/NotificationService.scala
@@ -0,0 +1,17 @@
+package au.com.zip.service
+
+import au.com.zip.encoders.CardAuthorizationResponse
+
+case class Notification(customerId: String,
+                        txnDate: String,
+                        cardAuthorizationResponse: CardAuthorizationResponse
+                       )
+
+object NotificationService {
+
+  //This would ideally be a microservice which lives outside of Kafka and is just invoked via the REST endpoint
+  def sendNotification(notification: Notification): Unit = {
+    println(s"Your transaction dated ${notification.txnDate} was processed with status ${notification.cardAuthorizationResponse.status} due to ${notification.cardAuthorizationResponse.reason}")
+  }
+
+}
